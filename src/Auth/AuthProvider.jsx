2be3536a8710f3eb58f2e6d9ed732d86acea7613
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-04-21 12:02:36 -0300

    Verificar autentificación

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,19 +1,97 @@
 import { useContext, createContext, useState, useEffect } from 'react'
+import { API_URL } from './constants';
 
 
 const AuthContext = createContext({
   isAuthenticated: false,
-  getAccessToken: () => {},
-  saveUser: (userData) => {},
-  getRefreshToken: () => {},
+  getAccessToken: () => { },
+  saveUser: (userData) => { },
+  getRefreshToken: () => { },
 });
 
 export function AuthProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [accessToken, setAccessToken] = useState('');
+  const [user, setUser] = useState('');
   // const [refreshToken, setRefreshToken] = useState('');
 
+  useEffect(() => { }, []);
 
+  async function requestNewAccessToken(refreshToken) {
+    try {
+      const response = await fetch(`${API_URL}/refresh-token`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `bearer ${refreshToken}`,
+        },
+      });
+      if (response.ok) {
+        const json = await response.json();
+
+        if (json.error) {
+          throw new Error(json.error);
+        }
+        return json.body.accessToken;
+      } else {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async function getUserInfo(accessToken) {
+    try {
+      const response = await fetch(`${API_URL}/user`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `bearer ${accessToken}`,
+        },
+      });
+      if (response.ok) {
+        const json = await response.json();
+
+        if (json.error) {
+          throw new Error(json.error);
+        }
+        return json;
+      } else {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async function checkAuth() {
+    if (accessToken) {
+      // el usuario esta autenticado
+
+    } else {
+      // el usuario no esta autenticado
+      const token = getRefreshToken();
+      if (token) {
+        const newAccesToken = await requestNewAccessToken(token);
+        if (newAccesToken) {
+          const userInfo = await getUserInfo(newAccesToken);
+          if (userInfo) {
+            saveSessionInfo(userInfo, newAccesToken, token);
+          }
+        }
+      }
+    }
+  }
+
+  function saveSessionInfo(userInfo, accessToken, refreshToken) {
+    setAccessToken(accessToken);
+    localStorage.setItem('token', JSON.stringify(refreshToken));
+    setIsAuthenticated(true);
+    setUser(userInfo);
+  }
 
   function getAccessToken() {
     return accessToken;
@@ -22,17 +100,18 @@ export function AuthProvider({ children }) {
   function getRefreshToken() {
     const token = localStorage.getItem('token');
     if (token) {
-      const {refreshToken} = JSON.parse(token);
+      const { refreshToken } = JSON.parse(token);
       return refreshToken;
     }
+    return null;
   }
 
   function saveUser(userData) {
-    setAccessToken(userData.body.accessToken);
-    // setRefreshToken(userData.body.refreshToken);
-
-    localStorage.setItem('token', JSON.stringify(userData.body.refreshToken));
-    setIsAuthenticated(true);
+    saveSessionInfo(
+      userData.body.user, 
+      userData.body.accessToken, 
+      userData.body.refreshToken
+    );
   }
 
 
