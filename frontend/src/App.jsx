cf7978b4ba019cb7655c5a34b7490b2de0c231c5
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-17 23:21:39 -0300

    Refactor: Estilo de modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import './App.css'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { AuthProvider } from "./context/AuthContext.jsx";
--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,7 +1,6 @@
-import './Modal.css'; // Estilos CSS para la modal
-
+import format from "../../helpers/format.js";
 const Modal = ({ setModalIsOpen, datosParaModal, handleConfirmation }) => {
-  const { newBalance, dataValid, saltoTotal } = datosParaModal;
+  const { newBalance, dataValid, saldoTotal } = datosParaModal;
 
   const handleAceptar = () => {
     setModalIsOpen(false);
@@ -13,19 +12,22 @@ const Modal = ({ setModalIsOpen, datosParaModal, handleConfirmation }) => {
     handleConfirmation(false); // Llamada a la función de confirmación con false
   };
 
-
   return (
-    <section className="modal">
-      <article className="modal-content">
-        <p>¿Estás seguro de que deseas agregar el siguiente gasto?</p>
-        <p>Título: {dataValid.title}</p>
-        <p>Descripción: {dataValid.description}</p>
-        <p>Balance: {dataValid.balance}</p>
-        <p>Sueldo actual: {saltoTotal}</p>
-        <p>Diferencia: {newBalance}</p>
-        <div className='botones'>
-          <button onClick={handleAceptar}>Aceptar</button>
-          <button onClick={handleCancelar}>Cancelar</button>
+    <section className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-40">
+      <article className="flex flex-col gap-3 bg-L-B-S dark:bg-D-B-P text-L-T-S p-6 rounded-lg w-11/12 max-w-md">
+        <h2 className="text-center text-3xl font-bold border-b-2 border-solid border-L-D-P">{dataValid.title}</h2>
+        <p className="text-center text-lg ">
+          ¿Estás seguro de que deseas agregar el siguiente {dataValid.title}?
+        </p>
+        <div className="flex flex-col gap-2 text-xl">
+          <p className="border-b-2 border-dashed border-L-B-P text-L-D-P font-semibold">Descripción: <span className="text-L-T-S font-rubik">{dataValid.description}</span></p>
+          <p className="border-b-2 border-dashed border-L-B-P text-L-D-P font-semibold">Balance: <span className="text-orange-600">$</span> <span className="text-L-T-S font-rubik">{dataValid.balance && format(dataValid.balance)}</span></p>
+          <p className="border-b-2 border-dashed border-L-B-P text-L-D-P font-semibold">Sueldo actual: <span className="text-orange-600">$</span> <span className="text-L-T-S font-rubik">{saldoTotal && format(saldoTotal)}</span></p>
+          <p className="border-b-2 border-dashed border-L-B-P text-L-D-P font-semibold">Diferencia: <span className="text-orange-600">$</span> <span className="text-L-T-S font-rubik">{newBalance && format(newBalance)}</span></p>
+        </div>
+        <div className="flex justify-end gap-4">
+          <button onClick={handleAceptar} className="px-4 py-2 bg-green-500 text-white rounded-lg">Aceptar</button>
+          <button onClick={handleCancelar} className="px-4 py-2 bg-red-500 text-white rounded-lg">Cancelar</button>
         </div>
       </article>
     </section>
