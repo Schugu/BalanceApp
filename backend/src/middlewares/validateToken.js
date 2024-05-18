a173daa70cc1f47ad74af4a416f8a8733bea69e3
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-18 17:33:23 -0300

    Fix: Error
    Cambie la forma en la que se muestra un error para que no se muestre en el front-end

--- backend/src/middlewares/validateToken.js
+++ backend/src/middlewares/validateToken.js
@@ -5,17 +5,21 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from '../config.js';
 
 // La funcion necesita estos 3 parametros para conciderarse un middleware
-  // el next significa que continue y que no retorne nada por que hay una 
-  // funcion que debe ejecutarse despues
-export const authRequire = (req, res, next)=> {
+// el next significa que continue y que no retorne nada por que hay una 
+// funcion que debe ejecutarse despues
+export const authRequire = (req, res, next) => {
   // Almacenar token de las cookies
   const { token } = req.cookies;
 
   // Si no hay token devolver un mensaje de error
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+  if (!token) {
+    console.log('No token, authorization denied');
+  
+    return res.status(401);
+  };
 
   // Verificar si el token es valido.
-  jwt.verify(token, TOKEN_SECRET, (err, user)=>{
+  jwt.verify(token, TOKEN_SECRET, (err, user) => {
     // Si el token es invalido lanzar un mensaje de error
     if (err) return res.status(401).json({ message: 'Invalid token' });
 
