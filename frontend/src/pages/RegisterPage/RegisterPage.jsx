9061c50ea4a04fae507aa7a8f0e44d4d960f9eb9
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-19 11:59:29 -0300

    Add: Diseño para PC del Register

--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -19,82 +19,85 @@ function RegisterPage() {
   });
 
   return (
-    <section className='w-full min-h-screen p-2.5 flex flex-col gap-2 place-items-center text-2xl bg-L-B-P dark:bg-D-B-P text-L-T-P dark:text-D-T-P'>
-      <article className='w-full flex flex-col items-center gap-4'>
-        <div className='w-48 h-48	rounded-full overflow-hidden border-4 border-solid border-L-D-P'>
-          <img className="w-full h-auto object-cover object-center" src="CarpinchoPlatudo.jpg" alt="CarpinchoPlatudo" />
+    <section className='w-full min-h-screen p-2 flex justify-center items-center text-2xl bg-L-B-P dark:bg-D-B-P text-L-T-P dark:text-D-T-P'>
+      <article className="lg:w-4/6 w-full h-full flex flex-col items-center justify-center gap-2">
+        <div className='w-5/6 flex flex-col items-center gap-4 border-b-2 border-solid border-L-D-P '>
+          <section className='w-52 h-52	rounded-full overflow-hidden border-4 border-solid border-L-D-P'>
+            <img className="w-full h-auto object-cover object-center" src="CarpinchoPlatudo.jpg" alt="CarpinchoPlatudo" />
+          </section>
+
+          <Link to='/' className='pb-1.5 text-center transition-colors duration-300 ease-in-out hover:text-L-D-P-dark dark:hover:text-L-D-P-light'>Carpincho Eficiente</Link>
         </div>
-        <Link to='/' className='pb-1.5 border-b-2 border-L-D-P w-4/5 text-center transition-colors duration-300 ease-in-out hover:text-L-D-P-light'>Carpincho Eficiente</Link>
-      </article>
 
-      <article className='w-full flex flex-col place-items-center gap-2.5'>
-        {
-          registerErrors.map((error, i) => (
-            <div key={i} className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded">
-              {error}
-            </div>
-          ))
-        }
+        <div className='w-full flex flex-col place-items-center gap-2.5'>
+          {
+            registerErrors.map((error, i) => (
+              <div key={i} className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded">
+                {error}
+              </div>
+            ))
+          }
 
-        <h3 className='px-2 text-center border-b-2 border-solid border-L-D-P'>Crear una cuenta</h3>
+          <h3 className='px-2 text-center border-b-2 border-solid border-L-D-P'>Crear una cuenta</h3>
 
-        <form className='w-full flex flex-col items-center gap-3.5' onSubmit={onSubmit}>
-        <section className='w-full flex flex-col items-center'>
-            <h4 className='w-9/12 text-xl'>Nombre de usuario</h4>
-            <input
-              type="text"
-              name="username"
-              placeholder='Carpincho Eficiente'
-              {...register('username', { required: true })}
-              className='w-9/12 text-base p-2.5 outline-none border-b-2 border-solid border-L-D-P bg-transparent hover:bg-L-D-P-hover rounded'
-            />
-            {
-              errors.username && (
-                <p className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded rounded">Username is requiere</p>
-              )
-            }
-          </section>
+          <form className='w-full flex flex-col items-center gap-3.5' onSubmit={onSubmit}>
+            <section className='w-full flex flex-col items-center'>
+              <h4 className='w-9/12 text-xl'>Nombre de usuario</h4>
+              <input
+                type="text"
+                name="username"
+                placeholder='Carpincho Eficiente'
+                {...register('username', { required: true })}
+                className='w-9/12 text-base p-2.5 outline-none border-b-2 border-solid border-L-D-P bg-transparent hover:bg-L-D-P-hover rounded'
+              />
+              {
+                errors.username && (
+                  <p className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded">Username is requiere</p>
+                )
+              }
+            </section>
 
-          <section className='w-full flex flex-col items-center'>
-            <h4 className='w-9/12 text-xl'>Correo electrónico</h4>
-            <input
-              type="email"
-              name="email"
-              placeholder='carpincho_eficiente@gmail.com'
-              {...register('email', { required: true })}
-              className='w-9/12 text-base p-2.5 outline-none border-b-2 border-solid border-L-D-P bg-transparent hover:bg-L-D-P-hover rounded'
-            />
-            {
-              errors.email && (
-                <p className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded rounded">Email is requiere</p>
-              )
-            }
-          </section>
+            <section className='w-full flex flex-col items-center'>
+              <h4 className='w-9/12 text-xl'>Correo electrónico</h4>
+              <input
+                type="email"
+                name="email"
+                placeholder='carpincho_eficiente@gmail.com'
+                {...register('email', { required: true })}
+                className='w-9/12 text-base p-2.5 outline-none border-b-2 border-solid border-L-D-P bg-transparent hover:bg-L-D-P-hover rounded'
+              />
+              {
+                errors.email && (
+                  <p className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded">Email is requiere</p>
+                )
+              }
+            </section>
 
-          <section className='w-full flex flex-col items-center'>
-            <h4 className='w-9/12 text-xl'>Contraseña</h4>
-            <input
-              type="password"
-              name="password"
-              placeholder='*********'
-              {...register('password', { required: true })}
-              className='w-9/12 text-base p-2.5 outline-none border-b-2 border-solid border-L-D-P bg-transparent hover:bg-L-D-P-hover rounded'
-            />
-            {
-              errors.password && (
-                <p className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded rounded">Password is requiere</p>
-              )
-            }
-          </section>
+            <section className='w-full flex flex-col items-center'>
+              <h4 className='w-9/12 text-xl'>Contraseña</h4>
+              <input
+                type="password"
+                name="password"
+                placeholder='*********'
+                {...register('password', { required: true })}
+                className='w-9/12 text-base p-2.5 outline-none border-b-2 border-solid border-L-D-P bg-transparent hover:bg-L-D-P-hover rounded'
+              />
+              {
+                errors.password && (
+                  <p className="w-9/12 bg-red-100 text-red-600 text-center p-1 rounded">Password is requiere</p>
+                )
+              }
+            </section>
 
-          <button type="submit" className='w-9/12 bg-L-D-P pt-1.5 no-underline transition-colors duration-300 ease-in-out border-b-4 border-solid border-L-D-P-dark hover:bg-L-D-P-dark hover:border-L-D-P rounded'>Registrarse</button>
+            <button type="submit" className='w-9/12 bg-L-D-P pt-1.5 no-underline transition-colors duration-300 ease-in-out border-b-4 border-solid border-L-D-P-dark hover:bg-L-D-P-dark hover:border-L-D-P rounded'>Registrarse</button>
 
-          <section className='flex flex-wrap justify-center items-center gap-2.5 text-center'>
-            <p className="text-xl">¿Ya tienes una cuenta?</p>
-            <Link to='/login' className='no-underline text-xl transition-colors duration-300 ease-in-out hover:text-L-D-P-dark dark:hover:text-L-D-P-dark'>Inicia Sesión</Link>
-          </section>
-        </form>
+            <section className='flex flex-wrap justify-center items-center gap-2.5 text-center'>
+              <p className="text-xl">¿Ya tienes una cuenta?</p>
+              <Link to='/login' className='no-underline text-xl transition-colors duration-300 ease-in-out hover:text-L-D-P-dark dark:hover:text-L-D-P-dark'>Inicia Sesión</Link>
+            </section>
+          </form>
 
+        </div>
       </article>
     </section>
   );
