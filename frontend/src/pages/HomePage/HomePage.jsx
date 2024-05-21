ff17d1098bde939029874e7f6395982801e9ab2c
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-21 16:57:46 -0300

    Add: Accesibilidad del homepage

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -4,15 +4,15 @@ import parrafos from "./parrafos.js";
 function HomePage() {
   return (
     <div className="flex flex-col items-center">
-      <Navbar></Navbar>
+      <Navbar></Navbar> 
 
       <section className="w-full min-h-screen flex flex-wrap gap-5 p-2.5 justify-center bg-L-B-P text-L-T-P dark:bg-D-B-P dark:text-D-T-P">
 
         {parrafos.map((parrafo, index) => (
           <article className="flex flex-col gap-2.5 s:max-w-lg" key={index}>
-            <h2 className="text-2xl border-b-2 border-dashed border-L-D-P-dark dark:border-D-D-P">{parrafo.titulo}</h2>
-            {(parrafo.parrafos).map((parrafo, index)=>(
-              <p className="text-lg text-wrap indent-5" key={index}>{parrafo}</p>
+            <p tabIndex={`1${index}1`} className="text-2xl border-b-2 border-dashed border-L-D-P-dark dark:border-D-D-P">{parrafo.titulo}</p>
+            {(parrafo.parrafos).map((parrafo, index2)=>(
+              <p tabIndex={`1${index}${2 + index2}`} className="text-lg text-wrap indent-5" key={index2}>{parrafo}</p>
             ))}
           </article>
         ))}
